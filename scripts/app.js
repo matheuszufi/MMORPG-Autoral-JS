25e7a9c34770598ccfa38f864f19421bfc6ab30f
Matheus Zufi <matheuszufi@gmail.com>
2022-06-16 11:36:35 -0300

    up

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,41 @@
 const character = document.getElementById('player');
 
+// PLAYER MOVIMENT
+    // PLAYER MOVIMENT
+        // PLAYER MOVIMENT
 
-
-
-
-
+let posY = 0;
+let posX = 0;
 document.addEventListener('keydown', (event) => {
     btnA = "a";
     btnW = "w";
     btnD = "d";
     btnS = "s";
-
-    let posX = 0;
-    let posY = 0;
  
    if(event.key === btnA) {
-    alert('funcionandos');
-   } else if (event.key === btnW) {
+        if(posX > 0){
+            posX = posX - 80;
+            character.style.transform = `translate(${posX}px,${posY}px)`
+        }
 
-   } else if (event.key === btnD) {
-    character.style.left = `${posX + 10}px`  
+    } else if (event.key === btnW) {
+        if(posY > 0){
+            posY = posY - 50;
+            character.style.transform = `translate(${posX}px,${posY}px)`
+        }
+
+    } else if (event.key === btnD) {
+        if(posX < 720){
+            posX = posX + 80;
+            character.style.transform = `translate(${posX}px,${posY}px)`                   
+        }
+   
    } else if (event.key === btnS) {
-       posY++
-    character.style.top = `${posY + 10}px`  
-   }
+        if(posY < 450){
+            posY = posY + 50;
+            character.style.transform = `translate(${posX}px,${posY}px)`
+   
+        }
+    }
 
 });
