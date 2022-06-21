669eba06d5a0974b1f460163bc6ffd6ed676d6e9
Matheus Zufi Leite <matheuszufi@gmail.com>
2022-06-20 23:32:07 -0300

    up spell store

--- scripts/shopping.js
+++ scripts/shopping.js
@@ -16,26 +16,93 @@ const cardIce = document.getElementById('card-ice');
 const cardUltimate = document.getElementById('card-ultimate');
 
 let isPowerFisicalOn = false;
+let isPowerFireOn = false;
+let isPowerEnergyOn = false;
+let isPowerTerraOn = false;
+let isPowerIceOn = false;
+let isPowerUltimateOn = false;
 
 
 cardFisical.addEventListener('click', buyFisicalPower);
+cardFire.addEventListener('click', buyFirePower);
+cardEnergy.addEventListener('click', buyEnergyPower);
+cardTerra.addEventListener('click', buyTerraPower);
+cardIce.addEventListener('click', buyIcePower);
+cardUltimate.addEventListener('click', buyUltimatePower);
 
 function buyFisicalPower() {
 
     if (player.coins >= 500) {
         player.coins -= 500;
+        charCoins.innerHTML = `${player.coins}`
         characterPowerFisical.style.display = "flex"
         isPowerFisicalOn = true;
-        cardFisical.style.opacity = "0.4"
-       
+        cardFisical.style.opacity = "0.4"  
     } else {
-        alert("It costs 500GP!d ")
+        alert("It costs 500GP! ")
     }
-
-
 };
 
+function buyFirePower() {
 
+    if (player.coins >= 500) {
+        player.coins -= 500;
+        charCoins.innerHTML = `${player.coins}`
+        characterPowerFire.style.display = "flex"
+        isPowerFireOn = true;
+        cardFire.style.opacity = "0.4"   
+    } else {
+        alert("It costs 500GP! ")
+    }
+};
+function buyEnergyPower() {
+
+    if (player.coins >= 500) {
+        player.coins -= 500;
+        charCoins.innerHTML = `${player.coins}`
+        characterPowerEnergy.style.display = "flex"
+        isPowerEnergyOn = true;
+        cardEnergy.style.opacity = "0.4"   
+    } else {
+        alert("It costs 500GP! ")
+    }
+};
+function buyTerraPower() {
+
+    if (player.coins >= 500) {
+        player.coins -= 500;
+        charCoins.innerHTML = `${player.coins}`
+        characterPowerTerra.style.display = "flex"
+        isPowerTerraOn = true;
+        cardTerra.style.opacity = "0.4"   
+    } else {
+        alert("It costs 500GP! ")
+    }
+};
+function buyIcePower() {
+
+    if (player.coins >= 500) {
+        player.coins -= 500;
+        charCoins.innerHTML = `${player.coins}`
+        characterPowerIce.style.display = "flex"
+        isPowerIceOn = true;
+        cardIce.style.opacity = "0.4"   
+    } else {
+        alert("It costs 500GP! ")
+    }
+};
+function buyUltimatePower() {
+
+    if (player.coins >= 2500) {
+        player.coins -= 2500;
+        charCoins.innerHTML = `${player.coins}`
+        characterPowerUltimate.style.display = "flex"
+        isPowerUltimateOn = true;
+        cardUltimate.style.opacity = "0.4"   
+    } else {
+        alert("It costs 2500GP! ")
+    }
+};
 
 
 
@@ -43,7 +110,7 @@ document.addEventListener('keydown', (event) => {
 
     btn2 = "2";
 
-    if (event.key === btn2 && isTag && player.magic > 30 && isPowerFisicalOn) {
+    if (event.key === btn2 && isTag && player.magic > 30 && isPowerFisicalOn && distanceX <= 100 && distanceX >= -100 && distanceY <= 160 && distanceY >= -160) {
 
     
             player.magic -= 30;
@@ -54,7 +121,74 @@ document.addEventListener('keydown', (event) => {
     };
 
 
+    btn3 = "3";
+
+    if (event.key === btn3 && isTag && player.magic > 30 && isPowerFireOn) {
+
+    
+            player.magic -= 30;
+            enemies.rat.life -= 10;
+            ratLife.style.width = `${(enemies.rat.life / enemies.rat.maxLife) * 100}%`
+            pOutputMana.innerHTML = `${player.magic}`
+            pBarMana.style.width = `${(player.magic / player.maxMagic) * 100}%`;
+    };
+
+    btn4 = "4";
+
+    if (event.key === btn4 && isTag && player.magic > 30 && isPowerEnergyOn) {
+
+    
+            player.magic -= 30;
+            enemies.rat.life -= 10;
+            ratLife.style.width = `${(enemies.rat.life / enemies.rat.maxLife) * 100}%`
+            pOutputMana.innerHTML = `${player.magic}`
+            pBarMana.style.width = `${(player.magic / player.maxMagic) * 100}%`;
+    };
+
+    btn5 = "5";
+
+    if (event.key === btn5 && isTag && player.magic > 30 && isPowerTerraOn) {
+
+    
+            player.magic -= 30;
+            enemies.rat.life -= 10;
+            ratLife.style.width = `${(enemies.rat.life / enemies.rat.maxLife) * 100}%`
+            pOutputMana.innerHTML = `${player.magic}`
+            pBarMana.style.width = `${(player.magic / player.maxMagic) * 100}%`;
+    };
+
+    
+    btn6 = "6";
+
+    if (event.key === btn6 && isTag && player.magic > 30 && isPowerIceOn) {
+
+    
+            player.magic -= 30;
+            enemies.rat.life -= 10;
+            ratLife.style.width = `${(enemies.rat.life / enemies.rat.maxLife) * 100}%`
+            pOutputMana.innerHTML = `${player.magic}`
+            pBarMana.style.width = `${(player.magic / player.maxMagic) * 100}%`;
+    };
+
+    btn7 = "7";
+
+    if (event.key === btn7 && isTag && player.magic > 30 && isPowerUltimateOn) {
+
+    
+            player.magic -= 30;
+            enemies.rat.life -= 20;
+            ratLife.style.width = `${(enemies.rat.life / enemies.rat.maxLife) * 100}%`
+            pOutputMana.innerHTML = `${player.magic}`
+            pBarMana.style.width = `${(player.magic / player.maxMagic) * 100}%`;
+    };
+
+
     if (enemies.rat.life < 1) {
         targetEnemy();
     }
+
+
+
+
+
 });
